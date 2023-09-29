b4ca60a45998e4a717ceca63bf88d8e0b4c67dca
Arnold <kasi.rock94@gmail.com>
2023-09-29 17:37:27 +0100

    margins and paddings updated

--- main.mobile.js
+++ main.mobile.js
@@ -1,9 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const menuIcon = document.getElementById("menu-icon");
-    const menuList = document.querySelector("ul");
-  
-    menuIcon.addEventListener("click", function () {
-      menuList.classList.toggle("hidden");
-    });
+  const menuIcon = document.getElementById("menu-icon");
+  const menuList = document.querySelector("ul");
+
+  // Initially hide the menu by setting its display to "none"
+  menuList.style.display = "none";
+
+  menuIcon.addEventListener("click", function () {
+      // Toggle the display property between "grid" and "none"
+      if (menuList.style.display === "grid") {
+          menuList.style.display = "none";
+      } else {
+          menuList.style.display = "grid";
+      }
   });
+});
+
   
